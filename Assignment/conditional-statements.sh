3e043f97f0487c0b48f0322c19d94b9a28de6965
uzairs786 <uzsamad@gmail.com>
2025-09-17 22:18:38 +0100

    Add script for conditional statements assignment

---
+++ Assignment/conditional-statements.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+if [ ! -f "$FILE" ]; then
+    echo "This file does not exist."
+else
+    echo "This file exists."
+    
+    if [ -r "$FILE" ]; then
+        echo "This file exists and is readable"
+    fi 
+    
+    if [ -w "$FILE" ]; then
+        echo "This file exists and is writeable"
+    fi 
+    
+    if [ -x "$FILE" ]; then 
+        echo "This file exists and is executable"
+    fi  
+fi
+
+
+
+
+
+
+
+
+
