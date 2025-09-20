cfaca594b925426e4ba4434e4447ebbc8ff478b3
Uzair Samad <85171652+uzairs786@users.noreply.github.com>
2025-09-20 16:11:45 +0100

    Update (optional task) selective-backup-script.sh

--- Assignment/(optional task) selective-backup-script.sh
+++ Assignment/(optional task) selective-backup-script.sh
@@ -5,12 +5,15 @@ inputfile_type() {
     
     echo "What type of file would you like to backup?"
     read filetype
+    
         if ls -R *."$filetype" 2>/dev/null; then
             mkdir -p Backup
-            cp *."$filetype" Backup/
+            cp *."$filetype" Backup/ || exit 1
             echo "All .$filetype files have been backed up."
+            exit 0
         else
             echo "No .$filetype does not exit."
+            exit 1
         fi
 }
 
